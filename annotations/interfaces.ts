02b67dd54d2aa04099ffd892019b60333f42ac42
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-21 00:28:51 -0600

    more interface

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -14,6 +14,15 @@ const oldCivic = {
   }
 };
 
+const drinkTwo = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
 const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
